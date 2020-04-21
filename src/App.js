5ab5bdb37955906aca46dea88a6b38c428b4b257
Michał Noszczyk <noszczykmichal@wp.pl>
2020-04-22 00:40:03 +0200

    1st point of assignment ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+
+  state={
+    length: null
+  }
+
+  lenghtChangedHandler = event =>{
+    const newLength = (event.target.value).length;
+
+    this.setState(
+      {length: newLength}
+    );
+  }
+ 
+  render(){
+    return (
+      <div className="App">
+        <input type="text" onChange={(event) => this.lenghtChangedHandler(event)}></input>
+        <p>{this.state.length}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
