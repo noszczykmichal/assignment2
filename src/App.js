6416b05e1103800cc001c5997e5dca09983b10a9
Michał Noszczyk <noszczykmichal@wp.pl>
2020-04-22 22:33:45 +0200

    4th point of assignment ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Validation from './Validation/Validation'
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
 class App extends Component{
 
@@ -24,6 +25,7 @@ class App extends Component{
         <input type="text" onChange={(event) => this.lenghtChangedHandler(event)}></input>
         <p>{this.state.length}</p>
         < Validation length={this.state.length}/>
+        <Char/>
       </div>
     );
   }
---
+++ src/Char/Char.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const char = (props)=>{
+    const style={
+        display: 'inline-block',
+        padding: '16px',
+        textAlign: 'center',
+        margin: '16px',
+        border: '1px solid black'
+    }
+
+    return <p style={style}></p>
+}
+
+export default char;
